1901456feed5358e990294aa32a80eb4c4454f7d
ViktoriiaVarivoda <victusia87@gmail.com>
2022-08-30 00:14:20 +0300

    дз9, задание1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
-
+const ref = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
+ref.buttonStop.disabled = true;
+let timerId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+ref.buttonStart.addEventListener('click', () => {
+  ref.buttonStart.disabled = true;
+  ref.buttonStop.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+ref.buttonStop.addEventListener('click', () => {
+    clearInterval(timerId);
+  ref.buttonStart.disabled = false;
+  ref.buttonStop.disabled = true;
+});
